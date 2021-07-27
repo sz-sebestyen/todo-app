ea5abddd12e1df86720cb787dab1f10d1879e0ee
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-07-27 13:25:30 +0200

    Move error handler into a middleware

--- todo-api/app.js
+++ todo-api/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
+const errorHandler = require("./middlewares/errorHandler");
 
 const indexRouter = require("./routes/index");
 
@@ -22,13 +23,6 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500).send("error");
-});
+app.use(errorHandler);
 
 module.exports = app;
