3f6d275d354b93a97a86306e17ba7461283e324f
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-07-27 13:15:55 +0200

    Add cors

--- todo-api/app.js
+++ todo-api/app.js
@@ -1,12 +1,14 @@
-var createError = require("http-errors");
-var express = require("express");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
-var indexRouter = require("./routes/index");
+const indexRouter = require("./routes/index");
 
-var app = express();
+const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
