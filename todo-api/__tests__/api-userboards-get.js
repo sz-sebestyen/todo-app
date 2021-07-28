4d8bc3555404071db8e2d1a6f231d260629ece92
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-07-28 14:54:18 +0200

    Test the response body

--- todo-api/__tests__/api-userboards-get.js
+++ todo-api/__tests__/api-userboards-get.js
@@ -39,5 +39,44 @@ describe("get api/userboards", () => {
 
       await res.expect(200);
     });
+
+    it("should return the userboard", async () => {
+      // given
+      const expected_user_id = "someUserId";
+
+      const expected_userboard = {
+        _id: expect.any(String),
+        user_id: expect.any(String),
+        dashboards: expect.anything(),
+      };
+
+      // when
+      const res = await request
+        .get("/api/userboards")
+        .set("Accept", "application/json")
+        .set("x-user_id", expected_user_id);
+
+      // then
+      expect(res.body).toMatchObject(expected_userboard);
+    });
+
+    it("should only create one userboard on the first two requests", async () => {
+      // given
+      const getUserboard = async () => {
+        await request
+          .get("/api/userboards")
+          .set("Accept", "application/json")
+          .set("x-user_id", "someUserId");
+      };
+
+      // when
+      await getUserboard();
+      await getUserboard();
+
+      const numberOfUserboards = await Userboard.countDocuments();
+
+      // then
+      expect(numberOfUserboards).toBe(1);
+    });
   });
 });
--- todo-api/routes/userboardRouter.js
+++ todo-api/routes/userboardRouter.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const Userboard = require("../models/Userboard");
 
-router.get("/", function (req, res, next) {
+router.get("/", async (req, res, next) => {
   const user_id = req.headers["x-user_id"];
-  if (user_id) {
-    res.json();
-  } else {
-    res.status(401).json({ message: "Unauthorized" });
-  }
+
+  if (!user_id) return res.status(401).json({ message: "Unauthorized" });
+
+  const userboard = await Userboard.findOneAndUpdate(
+    { user_id },
+    { user_id },
+    { upsert: true, new: true }
+  );
+
+  res.json(userboard);
 });
 
 module.exports = router;
