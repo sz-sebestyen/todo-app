c5a9034f831cf885effaba23a18f86d365994dca
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-08-02 14:47:48 +0200

    Refactor

--- api-gateway/routes/todoRouter.js
+++ api-gateway/routes/todoRouter.js
@@ -26,11 +26,11 @@ const getUserId = (auth = "") => {
 };
 
 proxy.on("proxyReq", function (proxyReq, req, res, options) {
+  proxyReq.removeHeader("X-user_id");
+
   const userId = getUserId(req.headers.authorization);
 
-  userId
-    ? proxyReq.setHeader("X-user_id", userId)
-    : proxyReq.removeHeader("X-user_id");
+  userId && proxyReq.setHeader("X-user_id", userId);
 });
 
 router.use("/*", async (req, res, next) => {
